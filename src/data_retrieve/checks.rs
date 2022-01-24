c366899338a0f7d88f1d26bcbc311aec25791cda
Jacky Song <dixinlily@hotmail.com>
2022-01-24 18:08:47 -0500

    Move download check functions into separate module

---
+++ src/data_retrieve/checks.rs
@@ -0,0 +1,12 @@
+use anyhow::Result as AnyhowResult;
+use std::env;
+use std::path::PathBuf;
+
+pub fn data_location() -> AnyhowResult<PathBuf> {
+    let folder = env::temp_dir().join("deepmath_data");
+    Ok(folder)
+}
+
+pub fn data_present() -> AnyhowResult<bool> {
+    Ok(true)
+}
